2751fc7fe5d92ec662ed64d20268f04484d471b2
mohit747 <32394960+mohit747@users.noreply.github.com>
2021-05-05 22:21:06 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask,request, url_for, redirect, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+model=pickle.load(open('XGBoost.pkl','rb'))
+
+
+@app.route('/')
+def hello_world():
+    return render_template("cropified.html")
+
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    int_features=[int(x) for x in request.form.values()]
+    final=[np.array(int_features)]
+    print(int_features)
+    print(final)
+    output=model.predict_proba(final)
+    
+    return render_template('cropified.html',pred='You should grow {} in your land'.format(output))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
